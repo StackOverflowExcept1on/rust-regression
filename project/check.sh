da915c24bc4b780d4590689c6678435fa5626d3e
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2024-08-24 14:12:40 +0300

    add binary size

--- project/check.sh
+++ project/check.sh
@@ -2,4 +2,5 @@
 die() { echo "$*" 1>&2 ; exit 1; }
 (pwd | grep $USER) || die "plz compile in /home/$USER/..."
 cargo build --release
+du --bytes ./target/release/libprogram.so
 LC_ALL=C.UTF-8 grep $USER ./target/release/libprogram.so && die "/home/$USER/... found in ELF :(" || true
